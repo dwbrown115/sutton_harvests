eee710ec4c9689682a6b8d19b4ac1cfd865b7b21
dwbrown115 <114196159+dwbrown115@users.noreply.github.com>
2023-12-12 16:27:01 -0600

    added ability to change user's password

--- src/firebase/firestore/updateData.ts
+++ src/firebase/firestore/updateData.ts
@@ -1,7 +1,7 @@
 import { firebase_app } from "../index";
 import { doc, updateDoc, getFirestore } from "firebase/firestore";
 
-export default async function updateData(collection: string, id:string, data: any) {
+export default async function updateData(collection: string, id: string, data: any) {
     const db = getFirestore(firebase_app);
     const contentRef = await doc(db, collection, id);
     try {
--- src/pages/AccountBarPages/accountPages/personalInfo/securitySettings/index.tsx
+++ src/pages/AccountBarPages/accountPages/personalInfo/securitySettings/index.tsx
@@ -1,14 +1,142 @@
+import { useState } from "react";
+import {
+  getAuth,
+  updatePassword,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 import { AccountBar, AccountSidebar } from "../../../../../components";
+import { firebase_app } from "../../../../../firebase";
+import { logOut } from "../../../../../firebase";
 
 export default function securitySettings() {
+  const auth = getAuth(firebase_app);
+  const router = useNavigate();
+  const user = auth.currentUser;
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [editPassword, setEditPassword] = useState(false);
+  const [error, setError] = useState("");
+
+  async function handleUpdatePasswordForm(e: any) {
+    e.preventDefault();
+    if (auth.currentUser?.email) {
+      const email = auth.currentUser.email;
+      const credential = EmailAuthProvider.credential(email, oldPassword);
+      await reauthenticateWithCredential(user!, credential)
+        .then(async () => {
+          if (newPassword === confirmNewPassword) {
+            await updatePassword(user!, newPassword)
+              .then(() => {
+                // alert("Password updated successfully!");
+                logOut();
+                router("/signin");
+              })
+              .catch((error) => {
+                setError(error.message);
+              });
+          } else {
+            setError("Passwords do not match!");
+          }
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    }
+  }
+
+  function handleUpdatePassword() {
+    return (
+      <div className="py-8 flex">
+        <div className="w-1/3 font-semibold">Password</div>
+        {editPassword ? (
+          <form
+            onSubmit={handleUpdatePasswordForm}
+            className="flex flex-col w-1/3"
+          >
+            <div>Update password your current password</div>
+            <div className="text-red mt-3">{error}</div>
+            <input
+              className="border border-black rounded-md px-4 py-2 mt-4 w-full bg-gray-50"
+              type="password"
+              onChange={(e) => setOldPassword(e.target.value)}
+              required
+              value={oldPassword}
+              id="oldPassword"
+              placeholder="Current password:"
+            />
+            <input
+              className="border border-black rounded-md px-4 py-2 mt-4 w-full bg-gray-50"
+              type="password"
+              onChange={(e) => setNewPassword(e.target.value)}
+              required
+              value={newPassword}
+              id="newPassword"
+              placeholder="New password:"
+            />
+            <input
+              className="border border-black rounded-md px-4 py-2 mt-4 w-full bg-gray-50"
+              type="password"
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+              required
+              value={confirmNewPassword}
+              id="confirmNewPassword"
+              placeholder="Confirm new password:"
+            />
+            <div className="mt-4 justify-between flex">
+              <button
+                type="button"
+                className="rounded-xl px-4 py-3 border border-sky-600 text-sky-600 hover:bg-sky-600 hover:text-white  w-12/25 text-sm"
+                onClick={() => {
+                  setEditPassword(false);
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="rounded-xl px-4 py-3 border border-gray-600 text-white bg-gray-600 hover:bg-transparent hover:text-gray-600 w-12/25 text-sm"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        ) : (
+          <div className="flex w-full">
+            <div>Update password your current password</div>
+            <div
+              className="text-blue hover:text-purple underline cursor-pointer ml-auto"
+              onClick={() => {
+                setEditPassword(true);
+              }}
+            >
+              Edit
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div>
       <AccountBar />
       <div className="mx-10">
         <div className="flex">
           <AccountSidebar />
-          <div>
-            <h1>Security Settings</h1>
+          <div className="w-full">
+            <div>
+              <h1 className="text-3xl font-semibold pb-6">
+                Sign in and Security
+              </h1>
+            </div>
+            <hr className="h-px" />
+            {handleUpdatePassword()}
+            <hr className="h-px" />
           </div>
         </div>
       </div>
